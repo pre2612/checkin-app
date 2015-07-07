153882b04e9fd26962d47e6dcf3a86bbbb87ccaa
preethy borrawar <preethy.borrawar@gmail.com>
2015-07-07 12:36:30 -0400

    added comment regarding firebase

--- src/js/global.js
+++ src/js/global.js
@@ -1,6 +1,7 @@
 "use strict";
 
 //single var pattern
+//Firebase is backend, including data storage, user authentication, static hosting
 var firebaseRef = new Firebase("https://checkin-checkout.firebaseio.com/"), //Define firebase global variable to access
     CheckIn = CheckIn || {}; //Global namespace for Entire app
     CheckIn.data = CheckIn.data || {};
